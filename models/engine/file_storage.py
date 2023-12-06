91558c6c3c2be4dd6802dfd8757ddc0fd9dd1627
Marve018 <122813162+Marve018@users.noreply.github.com>
2023-12-06 11:16:05 +0100

    changed the string formart method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,7 +31,7 @@ class FileStorage:
         """
             sets in __objects the obj with key <obj class name>.id
         """
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """
