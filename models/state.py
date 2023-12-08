1a1b314061e2e3b0b86f4ad7d23caa1ceefdb1f5
dadovenonye <chinonyenmecha@yahoo.com>
2023-12-07 19:03:05 -0800

    added the state class

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    The state class that inherits
+    from BaseModel
+"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+        public class state that inherits
+        from  BaseModel
+        Attr:
+        name: string - empty string
+    """
+    name = ""
